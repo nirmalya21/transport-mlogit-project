c9fec2a7645e6a8f093f1ccfc57eec655fbe4c0a
Nirmalya Rajpandit <nirmalya21@users.noreply.github.com>
2025-07-30 13:26:20 +0200

    Update app.R

--- app.R
+++ app.R
@@ -5,8 +5,6 @@ rsconnect::setAccountInfo(name='wupqf2-nirmalya-rajpandit',
 			  token='3745A1CA32B44606EFC92C3422D4B9E6',
 			  secret='AMQVBPkYBf8vScZGm1OFODrpmFTCf43LI5mH6GW1')
 
-library(rsconnect)
-    rsconnect::deployApp('transport_model')
 
-library(shiny)
-runGitHub("shiny-dashboard-sales", "nirmalya21")
+rsconnect::deployApp(appDir = ".")
+2
